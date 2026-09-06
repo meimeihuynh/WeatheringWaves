5defe77392dcd6c081f8a8136be5eb8c01ad7332
meimeihuynh <ngohuy02@innlandetfylke.no>
2026-09-06 19:46:44 +0200

    velge data

--- weatheringwaves/src/App.jsx
+++ weatheringwaves/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const location = "Test location";
 
   useEffect(() => {
     getWeather().then(data => {
@@ -17,12 +18,33 @@ function App() {
 
     <div>
         <h1>WeatheringWaves</h1>
- 
+        <h2>{location}</h2>
+
       {weatherData ? (
-        <p>Temperature:{" "}
+      <>
+         <p>Temperature:{" "}
         {weatherData.properties.timeseries[0].data.instant.details.air_temperature}
         °C
         </p> 
+
+        <p> Humidity:{" "}
+        {weatherData.properties.timeseries[0].data.instant.details.relative_humidity}
+        %
+        </p>
+
+        <p>
+        Wind speed:{" "}
+        {weatherData.properties.timeseries[0].data.instant.details.wind_speed}
+        m/s
+        </p>
+
+        <p>
+        Wind direction:{" "}
+        {weatherData.properties.timeseries[0].data.instant.details.wind_from_direction}
+        °
+        </p>
+
+      </>
       ) : (
         <p>loading weather...</p>)
       }
